1aee0025d17217f886cdd4488127e90c36ada669
Pavel <gerasimov86@gmail.com>
2019-11-12 17:37:03 +0300

    fix templates

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -11,7 +11,19 @@ def create_app():
         return render_template('index.html', page_title=title)
 
     @app.route('/patronage')
-    def base_test():
+    def patronage():
         return render_template('patronazh.html')
+
+    @app.route('/hospital')
+    def hospital():
+        return render_template('stacionar.html')
+
+    @app.route('/patronage_item')
+    def patronage_item():
+        return render_template('patronazh_item.html')
+
+    @app.route('/hospital_item')
+    def hospital_item():
+        return render_template('stacionar_item.html')
 
     return app
