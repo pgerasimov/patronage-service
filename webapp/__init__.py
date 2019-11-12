6973bca50dc607be0612568b511ba9be387f2bd9
Pavel <gerasimov86@gmail.com>
2019-11-12 18:28:35 +0300

    fix teamplates and routes

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -26,4 +26,22 @@ def create_app():
     def hospital_item():
         return render_template('stacionar_item.html')
 
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
+
+    @app.route('/send_form')
+    def send_form():
+        return 'ok'
+
+    @app.route('/login')
+    def login():
+        return render_template('login.html')
+
+    @app.route('/login_process')
+    def login_process():
+        return 'ok'
+
+
+
     return app
