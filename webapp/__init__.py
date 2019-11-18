3d0b14eb02e7258712d839093f0d75b3bb7a928c
Pavel <gerasimov86@gmail.com>
2019-11-18 18:20:38 +0300

    db

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from webapp.forms import ProfileForm
-from webapp.model import db
+from webapp.model import db, Worker
+
 
 def create_app():
     app = Flask(__name__)
@@ -46,9 +47,40 @@ def create_app():
     def login_process():
         return 'ok'
 
-    @app.route('/anketa')
-    def anketa():
+    @app.route('/worker')
+    def worker():
         form = ProfileForm()
-        return render_template('anketa.html', form=form)
+        return render_template('worker_profile.html', form=form)
+
+    @app.route('/add_worker', methods=['POST'])
+    def add_worker():
+        properties_map = {'medical': 0, 'recomendations': 0, 'diabet': 0, 'insult': 0, 'alzheimer': 0,
+                          'dcp': 0, 'bed': 0, 'oncology': 0, 'hygiene': 0, 'injections': 0, 'dropper': 0, 'lfk': 0,
+                          'cooking': 0, 'buyfood': 0, 'cleaning': 0, 'walking': 0}
+
+        all_args = request.form.to_dict()
+        all_args.pop('csrf_token')
+
+        properties = request.form.getlist("properties")
+
+        client_age = request.form.getlist("client-age")
+        client_age = ', '.join(client_age)
+
+        for item in properties:
+            if item in properties_map.keys():
+                properties_map[item] = 1
+
+        print(all_args)
+        print(f'Возраст клиента - {client_age}')
+        print(f"Опции чекбоксов - {properties_map}")
+
+        new_worker = Worker(surname=all_args['surname'], name=all_args['name'], mname=all_args['mname'],
+                            age=all_args['age'], bio=all_args['bio'], phone=all_args['phone'],
+                            email=all_args['email'], pricefrom=all_args['pricefrom'], priceto=all_args['priceto'],
+                            experience=all_args['experience'], shedule=all_args['shedule'], gender=all_args['sex'])
+        db.session.add(new_worker)
+        db.session.commit()
+
+        return 'ok'
 
     return app
--- webapp/forms.py
+++ webapp/forms.py
@@ -1,41 +1,9 @@
-from flask_wtf import FlaskForm, validators
-from wtforms import StringField, SubmitField, DateField, SelectField, TextAreaField
+from flask_wtf import FlaskForm
+
+
+class User(FlaskForm):
+    pass
 
 
 class ProfileForm(FlaskForm):
-    lastname = StringField(
-        'Фамилия*',
-        render_kw={
-            "class": "form-control",
-            "type": "text"})
-
-    name = StringField(
-        'Имя*',
-        render_kw={
-            "class": "form-control",
-            "type": "text"})
-
-    fathername = StringField(
-        'Отчетство',
-        render_kw={
-            "class": "form-control",
-            "type": "text"})
-
-    birthdate = DateField(
-        'Дата рождения*',
-        render_kw={
-            "class": "form-control date",
-        })
-
-    gender = SelectField(choices=[('1', 'Мужской'), ('2', 'Женский')])
-
-    bio = TextAreaField(u'Mailing Address'
-                        'О себе',
-                        render_kw={
-                            "class": "form-control",
-                            "type": "text",
-                            "placeholder": "Расскажите кратко о себе и своем опыте"})
-
-    submit_search = SubmitField(
-        'Зарегистрироваться',
-        render_kw={"class": "btn btn-primary", "type": "submit"})
+    pass
